24ff30dc8e6f2b3d688929c095fd89c3941ca6b6
hanlee <me@hanlee.co>
2021-04-15 12:23:22 +0900

    fix: process.env replacement on build

--- instance/axios.js
+++ instance/axios.js
@@ -1,10 +1,8 @@
 import Axios from 'axios';
 
-const { API_KEY, SERVICE_ID } = process.env;
-
 const api = Axios.create({
-  baseURL: `https://${SERVICE_ID}.microcms.io/api/v1/`,
-  headers: { 'X-API-KEY': API_KEY },
+  baseURL: `https://${process.env.SERVICE_ID}.microcms.io/api/v1/`,
+  headers: { 'X-API-KEY': process.env.API_KEY },
 });
 
 export default api;
