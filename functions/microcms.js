7f5b3de1834f5833a8970f062daace1357e35726
Kazuki Shibata <k.shibata97@gmail.com>
2021-09-10 10:49:06 +0900

    [fix] 相対パスでインポートしない

--- functions/microcms.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-require('dotenv').config();
-const { API_KEY, SERVICE_ID } = process.env;
-export const client = createClient({
-  serviceDomain: SERVICE_ID,
-  apiKey: API_KEY,
-});
--- functions/search.js
+++ functions/search.js
@@ -1,4 +1,10 @@
-import { client } from './microcms';
+import { createClient } from 'microcms-js-sdk';
+require('dotenv').config();
+const { API_KEY, SERVICE_ID } = process.env;
+const client = createClient({
+  serviceDomain: SERVICE_ID,
+  apiKey: API_KEY,
+});
 
 // eslint-disable-next-line require-await
 exports.handler = async (event) => {
