bc2c506f904617a8cdd5825378ff20e2bb2ac6f8
Kazuki Shibata <k.shibata97@gmail.com>
2021-09-10 11:15:06 +0900

    [update] 処理を共通化

--- functions/search.js
+++ functions/search.js
@@ -1,10 +1,4 @@
-const { createClient } = require('microcms-js-sdk');
-require('dotenv').config();
-const { API_KEY, SERVICE_ID } = process.env;
-const client = createClient({
-  serviceDomain: SERVICE_ID,
-  apiKey: API_KEY,
-});
+const { client } = require('../utils/microcms');
 
 // eslint-disable-next-line require-await
 exports.handler = async (event) => {
