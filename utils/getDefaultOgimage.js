d9eac72c92dd50f1906725653ddfecba3d4bb865
Matsuda Shoichi <matsuda@microcms.co.jp>
2022-11-21 18:46:34 +0900

    自動生成画像の文字位置を天地中央に変更

--- utils/getDefaultOgimage.js
+++ utils/getDefaultOgimage.js
@@ -1,8 +1,8 @@
 export default function getDefaultOgimage(content) {
   const encodedTitle = encodeURI(content.title);
   const textSize = content.title.length > 22 ? 40 : 60; // 正確な文字数ではないが大体の指標としては十分と判断する
-  return `https://images.blog.microcms.io/assets/f5d83e38f9374219900ef1b0cc4d85cd/92c09085ec6243cca78046fa644dd8cd/banner-bg.png?blend-mode=normal&blend-x=88&blend-y=231&blend64=${Buffer.from(
-    `https://assets.imgix.net/~text?txtsize=${textSize}&w=672&h=180&txtfont=Hiragino%20Sans%20W8&txt-color=212149&txt=${encodedTitle}`,
+  return `https://images.blog.microcms.io/assets/f5d83e38f9374219900ef1b0cc4d85cd/92c09085ec6243cca78046fa644dd8cd/banner-bg.png?blend-mode=normal&blend-x=88&blend-align=middle&blend64=${Buffer.from(
+    `https://assets.imgix.net/~text?txtsize=${textSize}&w=672&txtfont=Hiragino%20Sans%20W8&txt-color=212149&txt=${encodedTitle}`,
     'ascii'
   ).toString('base64')}`;
 }
