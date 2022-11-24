035e56ffbbcd4b080fe501b6b1cea17637e92e4e
Matsuda Shoichi <matsuda@microcms.co.jp>
2022-11-24 18:15:04 +0900

    Fix text size

--- utils/getDefaultOgimage.js
+++ utils/getDefaultOgimage.js
@@ -1,6 +1,7 @@
 export default function getDefaultOgimage(content) {
   const encodedTitle = encodeURI(content.title);
-  const textSize = content.title.length > 22 ? 52 : 60; // 正確な文字数ではないが大体の指標としては十分と判断する
+  const length = content.title.length;
+  const textSize = length > 30 ? 42 : length > 22 ? 52 : 60; // 正確な文字数ではないが大体の指標としては十分と判断する
   return `https://images.blog.microcms.io/assets/f5d83e38f9374219900ef1b0cc4d85cd/92c09085ec6243cca78046fa644dd8cd/banner-bg.png?blend-mode=normal&blend-x=88&blend-align=middle&blend64=${Buffer.from(
     `https://assets.imgix.net/~text?txtsize=${textSize}&w=672&txtfont=Hiragino%20Sans%20W8&txt-color=212149&txt=${encodedTitle}`,
     'ascii'
