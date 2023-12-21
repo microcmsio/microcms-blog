9addcd08b5d2a1eaf6b8c799e5bc05e7a7f00ddd
shimotsu <shimotsu13@gmail.com>
2023-12-21 16:06:48 +0900

    1年以上前の記事にアラートを表示する

---
+++ components/Alert.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-if="isOldPost && isTechnicalPost" class="alert">
+    <p>
+      この記事は公開後、1年以上経過しています。情報が古い可能性がありますので、ご注意ください。
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isOldPost: {
+      type: Boolean,
+      required: true,
+    },
+    isTechnicalPost: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.alert {
+  position: relative;
+  background-color: #faf6da;
+  color: var(--color-text-main);
+  margin-bottom: 40px;
+  padding: 16px 16px 18px 44px;
+  font-size: 14px;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    background: url('/images/icon_alert.svg') center no-repeat;
+    background-size: contain;
+    width: 18px;
+    height: 18px;
+    position: absolute;
+    top: 50%;
+    left: 16px;
+    transform: translateY(-50%);
+  }
+}
+</style>
